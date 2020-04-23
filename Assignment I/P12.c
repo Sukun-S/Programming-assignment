7fd30fc05a5b6edd1c2a4f485e2f811b094ac38d
Suman Mishra <40954938+smishra997@users.noreply.github.com>
2020-04-23 15:19:56 +0530

    Update P12.c

--- Assignment I/P12.c
+++ Assignment I/P12.c
@@ -1,10 +1,16 @@
 #include <stdio.h> 
 int main() 
 { 
-    int x = 4, y = 9; 
-    x = x + y; // x now becomes 15 
-    y = x - y; // y becomes 10 
-    x = x - y; // x becomes 5 
+    
+     double x, y;
+    printf("Enter a: ");
+    scanf("%lf", &a);
+    printf("Enter b: ");
+    scanf("%lf", &b);
+    
+    x = x + y; // x now becomes  
+    y = x - y; // y becomes  
+    x = x - y; // x becomes 
    printf("Results after changing values: x = %d, y = %d", x, y); 
    return 0; 
 }
