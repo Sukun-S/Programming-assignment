ca8c9081dca09e0abfce9ca87bb60bd672c5d771
Suman Mishra <40954938+smishra997@users.noreply.github.com>
2020-04-24 17:42:11 +0530

    Update A6.c

--- Assignment II/A6.c
+++ Assignment II/A6.c
@@ -1,89 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
 
- 
+int main(){
 
-unsigned long amount=1000, deposit, withdraw;
+int moey[]={1000,900,800,700,600,500,400,300,200,100,0};
+int withdraw_amount=0;
+int i=0,r;
+printf("enter amount");
+scanf("%d",&withdraw_amount);
+while(withdraw_amount>0){
+	
+r=withdraw_amount/money[i]; //money divides
+withdraw_amount = withdraw_amount%money[i]; // value of money returns
 
-int choice, k;
+printf("collect cash %4d  rupees\n", money[i],r);
+i++;
+}
 
-char transaction ='y';
-
- 
-
-int main()
-
-{
-
-
-		printf("2. Withdraw Cash\n");
-
-		printf("4. Quit\n");
-
-		printf("******************?**************************?*\n\n");
-
-		printf("Enter your choice: ");
-
-		scanf("%d", &choice);
-
-		switch (choice)
-
-		{
-
-		case 1:
-
-			printf("\n ENTER THE AMOUNT TO WITHDRAW: ");
-
-			scanf("%lu", &withdraw);
-
-			if (withdraw % 100 != 0)
-
-			{
-
-				printf("\n PLEASE ENTER THE AMOUNT IN MULTIPLES OF 100");
-
-			}
-
-			else if (withdraw >(amount - 500))
-
-			{
-
-				printf("\n INSUFFICENT BALANCE");
-
-			}
-
-			else
-
-			{
-
-				amount = amount - withdraw;
-
-				printf("\n\n PLEASE COLLECT CASH");
-
-				printf("\n YOUR CURRENT BALANCE IS%lu", amount);
-
-			}
-
-			break;
-
-		
-		default:
-
-			printf("\n INVALID CHOICE");
-
-		}
-
-		printf("\n\n\n DO U WISH TO HAVE ANOTHER TRANSCATION?(y/n): \n");
-
-		fflush(stdin);
-
-		scanf("%c", &transaction);
-
-		if (transaction == 'n'|| transaction == 'N')
-
-                    k = 1;
-
-	} while (!k);
-
-	printf("\n\n THANKS FOR USING OUT ATM SERVICE");
-
+printf("final value");
+	
+	return 0;
+	
 }
