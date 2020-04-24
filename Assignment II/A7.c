a39fcc8220a2789a53fc4f345e8d300a4807e561
Suman Mishra <40954938+smishra997@users.noreply.github.com>
2020-04-24 18:32:28 +0530

    Update A7.c

--- Assignment II/A7.c
+++ Assignment II/A7.c
@@ -1,24 +1,28 @@
-#include <stdio.h>
-int main() {
-    int num, originalNum, remainder, result = 0;
-    printf("Enter a three-digit integer: ");
-    scanf("%d", &num);
-    originalNum = num;
 
-    while (originalNum != 0) {
-        // remainder contains the last digit
-        remainder = originalNum % 10;
+
+#include <stdio.h>
+
+int main() {
+     int n, n1, rem, num = 0;
+    
+    printf("Enter number ");
+    scanf("%d", &n);
+    n1 = n;
+
+     while (n1 != 0) { // if value is zero then it doesnot satisfy the condition
         
-        result += remainder * remainder * remainder;
+      rem = n1 % 10; //rem stores the value of rem like 153%10 = quo = 15, rem = 3
         
-        // removing last digit from the orignal number
-        originalNum /= 10;
+        num += rem * rem * rem; // rem = 3*3*3; and add with num
+        
+        
+         n1/= 10; // here to check the quotient like 153/10, what is the quotient because already we got the last number
     }
 
-    if (result == num)
-        printf("%d is an Armstrong number.", num);
+    if (num == n) //after adding if we the same number then we can say its armstrong
+        printf("%d armstrong.", num);
     else
-        printf("%d is not an Armstrong number.", num);
+        printf("%d not Armstrong.", num);
 
     return 0;
 }
