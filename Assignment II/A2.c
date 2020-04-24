6e565d3eafdec5c85445f22756d2a8aa43ca7989
Suman Mishra <40954938+smishra997@users.noreply.github.com>
2020-04-24 07:31:26 +0530

    Update A2.c

--- Assignment II/A2.c
+++ Assignment II/A2.c
@@ -1,20 +1,27 @@
+
+    // A year is a leap year if it is divisible by 4 but not by 100.
+    // If a year is divisible by both 4 and by 100, then it can only be a leap year if it is also divisible by 400.
+
 #include <stdio.h>
-int main() {
-    int year;
-    printf("Enter a year: ");
-    scanf("%d", &year);
-    if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            // the year is a leap year if it is divisible by 400.
-            if (year % 400 == 0)
-                printf("%d is a leap year.", year);
-            else
-                printf("%d is not a leap year.", year);
-        } else
-            printf("%d is a leap year.", year);
-    } else
-        printf("%d is not a leap year.", year);
 
-    return 0;
+int main()
+{
+  int year;
+ 
+  printf("Enter a year\n"); 
+  scanf("%d", &year);
+    
+  if (year%400 == 0) // is leap
+    printf("%d a leap year.\n", year);
+   
+  else if (year%4 == 0) // is leap
+    printf("%d a leap year.\n", year);
+    
+      else if (year%100 == 0) // not leap
+    printf("%d not leap year.\n", year);
+    
+  else // Not leap
+    printf("%d not leap year.\n", year);  
+   
+  return 0;
 }
-
