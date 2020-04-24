ee4cb8840c659fe2bb1ef2fc3ab4d8fb006a82eb
Suman Mishra <40954938+smishra997@users.noreply.github.com>
2020-04-24 17:09:44 +0530

    Update A5.c

--- Assignment II/A5.c
+++ Assignment II/A5.c
@@ -1,45 +1,33 @@
 
 
+//roots of a quadratic equation
 
-#include <stdio.h>
-#include <math.h> // saw this from a tutorial and did
+//a*x*x+b*x+c=0;
+//root1,root2=sqrt(b*b- + 4*a*c)/2*a;
 
+#include<stdio.h>
+#include<math.h>
 
 int main()
 {
-    float a, b, c;
-    float root1, root2, imaginary;
-    float discriminant;
     
-    printf("Enter values of a, b, c of quadratic equation (aX^2 + bX + c): ");
-    scanf("%f%f%f", &a, &b, &c);
+    float a,b,c;
+    float root1,root2;
+    float root_part, denom;
     
-    /* Find discriminant of the equation */
-    discriminant = (b * b) - (4 * a * c);
     
-   
-    /* Find the nature of discriminant */
-    if(discriminant > 0)
-    {
-        root1 = (-b + sqrt(discriminant)) / (2*a);
-        root2 = (-b - sqrt(discriminant)) / (2*a);
-
-        printf("Two distinct and real roots exists: %.2f and %.2f", root1, root2);
-    }
-    else if(discriminant == 0)
-    {
-        root1 = root2 = -b / (2 * a);
-
-        printf("Two equal and real roots exists: %.2f and %.2f", root1, root2);
-    }
-    else if(discriminant < 0)
-    {
-        root1 = root2 = -b / (2 * a);
-        imaginary = sqrt(-discriminant) / (2 * a);
-
-        printf("Two distinct complex roots exists: %.2f + i%.2f and %.2f - i%.2f", 
-                root1, imaginary, root2, imaginary);
-    }
-
+    root_part = sqrt(b*b- 4*a*c); //if a equaltion's denominaor is 0 then its not quadratic its linear.
+    denom= 2*a;
+    
+    
+   root1= (-b+root_part)/denom;
+   root2 =(-b-root_part)/denom;
+    
+    printf("Roots= %f\n  Root2=%f", root1, root2);
+    
     return 0;
+    
+    
+    
+    
 }
