5ab3695271e843c34eaa9aebb1ff07eba61fc535
Suman Mishra <40954938+smishra997@users.noreply.github.com>
2020-04-30 12:57:16 +0530

    Add files via upload

--- Assignment II/Aa9.c
+++ Assignment II/Aa9.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    float basic, gross, da, hra;
+    float basic, gross, da, hra, np, tax;
 
     /* Input basic salary of employee */
     printf("Enter basic salary of an employee: ");
@@ -10,26 +10,38 @@ int main()
 
 
     /* Calculate D.A and H.R.A according to specified conditions */
-    if(basic <= 10000)
+    if(basic <= 12000)
     {
         da  = basic * 0.8;
         hra = basic * 0.2;
+ 	tax=0;
+ 	gross=basic+hra+da;
+ 	np=gross-tax;
+ 	printf("The gross pay is %f",gross);
+ 	printf("The net pay is %f",np);
     }
-    else if(basic <= 20000)
+    else if((basic >= 12000)&&(basic<30000))
     {
         da  = basic * 0.9;
         hra = basic * 0.25;
+	gross=basic+hra+da;
+	tax=basic * 0.12;
+ 	np=gross-tax;
+ 	printf("The gross pay is %f",gross);
+ 	printf("The net pay is %f",np);
     }
     else
     {
         da  = basic * 0.95;
         hra = basic * 0.3;
+	gross=basic+hra+da;
+	tax = basic*0.15;
+ 	np=gross-tax;
+ 	printf("The gross pay is %f\n",gross);
+ 	printf("The net pay is %f\n",np);
     }
 
-    /* Calculate gross salary */
-    gross = basic + hra + da;
-
-    printf("GROSS SALARY OF EMPLOYEE = %.2f", gross);
+  
 
     return 0;
 }
