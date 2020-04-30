d9f64db553d38a46206b79a7ff01cfad3c5d30e2
Suman Mishra <40954938+smishra997@users.noreply.github.com>
2020-04-30 15:37:40 +0530

    Update A6.c

--- Assignment II/A6.c
+++ Assignment II/A6.c
@@ -1,23 +1,17 @@
 #include<stdio.h>
+int main()
+{
+int a[10]={1000,900,800,700,600,500,400,300,200,100},m,temp,i; 
+printf("enter your money ");
+scanf("%d",&m);
+if(m%100==0){
+temp =m;
+(i=0;i<10;i++)
 
-int main(){
+printf("print",a[i],temp/a[i]);
+ temp=temp%a[i];
 
-int moey[]={1000,900,800,700,600,500,400,300,200,100,0};
-int withdraw_amount=0;
-int i=0,r;
-printf("enter amount");
-scanf("%d",&withdraw_amount);
-while(withdraw_amount>0){
-	
-r=withdraw_amount/money[i]; //money divides
-withdraw_amount = withdraw_amount%money[i]; // value of money returns
-
-printf("collect cash %4d  rupees\n", money[i],r);
-i++;
-}
-
-printf("final value");
-	
-	return 0;
-	
+else
+printf("Invalid amount not multiple of 100.\n ");
+return 0;
 }
