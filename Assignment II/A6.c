a1dcfc36a71693e64807a24ba9460b346f59a72e
Suman Mishra <40954938+smishra997@users.noreply.github.com>
2020-04-30 15:41:47 +0530

    Update A6.c

--- Assignment II/A6.c
+++ Assignment II/A6.c
@@ -1,16 +1,13 @@
 #include<stdio.h>
 int main()
 {
-int a[10]={1000,900,800,700,600,500,400,300,200,100},m,temp,i; 
+int a[10]={1000,900,800,700,600,500,400,300,200,100}; 
 printf("enter your money ");
 scanf("%d",&m);
 if(m%100==0){
-temp =m;
-(i=0;i<10;i++)
 
-printf("print",a[i],temp/a[i]);
- temp=temp%a[i];
-
+printf("print",a[m]);
+}
 else
 printf("Invalid amount not multiple of 100.\n ");
 return 0;
